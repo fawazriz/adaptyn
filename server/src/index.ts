00d2d5db5c0496c785eafc3ffa7c2c9ef58ea62e
fawazriz <fawazriz2019@gmail.com>
2026-04-22 01:49:06 -0400

    Initial project setup

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
